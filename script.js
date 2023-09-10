502f11a0ee80bbf6ec39930e65686991ed2600ef
OleJG <flucode9@gmail.com>
2023-09-10 18:10:56 -0400

    added index/js/stylecss

---
+++ script.js
@@ -0,0 +1,73 @@
+// Assignment Code
+document.addEventListener("DOMContentLoaded", function () {
+    const generateButton = document.getElementById("generate");
+    const passwordField = document.getElementById("password");
+});
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+generateBtn.addEventListener("click", writePassword);
+
+document.addEventListener("click", function () {
+    const generateButton = document.getElementById("generate");
+    const passwordField = document.getElementById("password");
+  
+    generateButton.addEventListener("click", function () {
+        const passwordLength = prompt("Enter the length of the password (between 8 and 128 characters):");
+        
+        if (passwordLength === null || passwordLength === "") {
+            alert("Please enter a valid password length.");
+            return;
+        }
+  
+        const length = parseInt(passwordLength);
+  
+        if (isNaN(length) || length < 8 || length > 128) {
+            alert("Please enter a valid password length between 8 and 128 characters.");
+            return;
+        }
+  
+        const includeLowercase = confirm("Include lowercase characters?");
+        const includeUppercase = confirm("Include uppercase characters?");
+        const includeNumbers = confirm("Include numbers?");
+        const includeSpecialChars = confirm("Include special characters?");
+  
+        if (!(includeLowercase || includeUppercase || includeNumbers || includeSpecialChars)) {
+            alert("Please select at least one character type to include.");
+            return;
+        }
+  
+        const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+        const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const numericChars = "0123456789";
+        const specialChars = "!@#$%^&*()_-+=<>?";
+  
+        let validChars = "";
+  
+        if (includeLowercase) {
+            validChars += lowercaseChars;
+        }
+  
+        if (includeUppercase) {
+            validChars += uppercaseChars;
+        }
+  
+        if (includeNumbers) {
+            validChars += numericChars;
+        }
+  
+        if (includeSpecialChars) {
+            validChars += specialChars;
+        }
+  
+        
+    });
+  });
